d5c56456e5ed7f42f7019f98501328888432ff3b
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-10 18:39:58 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,8 +2,8 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
 array.count do |index|
   index == String
-  end
-  index
+  p index
+end
 end
 
 
