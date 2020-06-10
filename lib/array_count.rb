40eff50be8a4cf1810aabe8a39bbd1a1fd753927
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-10 18:50:31 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -4,9 +4,9 @@ array.count do |index|
   end
 end
 
-
-
-
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count do |index|
+    index.class == String do
+      if index == " "
+    end
 end
